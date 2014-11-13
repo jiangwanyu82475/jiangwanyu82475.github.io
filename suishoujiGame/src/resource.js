932302fa35eb1f6bcf9b49393935a7d16d996aff
aiuu <116660998@qq.com>
2014-11-14 01:27:21 +0800

    ver1.0

---
+++ suishoujiGame/src/resource.js
@@ -0,0 +1,38 @@
+var s_HelloWorld = "HelloWorld.jpg";
+var s_back="img_back.png";
+var s_kaishi="img_btn_kaishi.png";
+var s_zailaiyici="img_btn_zailaiyici.png";
+var s_catch="img_catch.png";
+var s_jiayou="img_jiayou.png";
+var s_pojilu="img_pojilu.png";
+var s_point="img_point_01.png";
+var s_result="img_result.png";
+var s_icon="img_icon.png";
+var s_icon_bg="img_icon_bg.png";
+var s_tips1="img_tips1.png";
+var s_tips2="img_tips2.png";
+
+var g_resources = [
+    //image
+    {src:s_HelloWorld},
+    {src:s_back},
+    {src:s_kaishi},
+    {src:s_zailaiyici},
+    {src:s_catch},
+    {src:s_jiayou},
+    {src:s_pojilu},
+    {src:s_point},
+    {src:s_result},
+    {src:s_icon_bg},
+    {src:s_tips1},
+    {src:s_tips2},
+    //plist
+    {src:s_icon}
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
